6496a1f7d15441225b48b8fe375fdbcd13456163
Davide Galletti <d.galletti99@gmail.com>
2023-05-25 20:54:53 +0200

    Sistemate animazioni

--- index.js
+++ index.js
@@ -14,8 +14,14 @@ async function loadData() {
   return await d3.json("./data/dataset.json");
 };
 
-// function to make the chart
-function makeChart(svg, stackedData, x, y, colorMap) {
+
+function drawChart(stackedData, x, y, colorMap) {
+  svg = d3.select("#stacked-bars");
+
+  // set the dimensions and margins of the graph
+  svg.attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
   // ogni barra è dedicata ad un data-point 
   // ed è costituita dalla sovrapposizione delle cinque 
   // barre relative alle cinque variabili del data-point
@@ -24,20 +30,21 @@ function makeChart(svg, stackedData, x, y, colorMap) {
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")")
 
-
   // add X axis
   chart.append("g")
+    .attr("id", "x-axis")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x).tickSizeOuter(0));
 
   // add Y axis (invertito per via della convenzione utilizzata)
   chart.append("g")
+    .attr("id", "y-axis")
     .call(d3.axisLeft(y));
+  
+  series = chart.append("g").attr("id", "series");
 
   // Show the bars
-  chart.append("g")
-    .attr("id", "series")
-    .selectAll("g")
+  series.selectAll("g")
     // Enter in the stack data = loop key per key = group per group
     .data(stackedData)
     .enter().append("g")  
@@ -51,48 +58,85 @@ function makeChart(svg, stackedData, x, y, colorMap) {
         .attr("x", function(_, i) { return x(i); })
         .attr("y", function(d) { return y(d[1]); })
         .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-        .attr("hover", function(d) { return d; })
         .attr("width", x.bandwidth());
-};
+  
+  series.selectAll("g").exit().remove();
+}
 
-
-function makeLegend(svg, labels, colorMap) {
+function makeLegend(labels, colorMap) {
+  svg = d3.select("#stacked-bars");
   legend = svg.append("g")
     .attr("id", "legend")
     .attr("transform",
           "translate(" + (width + margin.left) + "," + (margin.top) + ")");
   // Add one dot in the legend for each name.
-  legend.selectAll("dots")
+  legend.selectAll("circle")
     .data(labels)
-    .enter()
-    .append("circle")
-      .attr("cx",0)
-      .attr("cy", function(_,i){ return i*25})
+    .enter().append("circle")
+      .attr("class", "dot")
+      .attr("cx", 0)
+      .attr("cy", function(_,i){ return (labels.length -1) * 25 - i*25;})
       .attr("r",  4)
-      .style("fill", function(d){ return colorMap(d)});
+      .attr("fill", function(d){ return colorMap(d)});
+  
+  legend.selectAll(".dot").exit().remove();
 
   // Add one dot in the legend for each name.
-  legend.selectAll("labels")
+  legend.selectAll("text")
     .data(labels)
-    .enter()
-    .append("text")
+    .enter().append("text")
+      .attr("class", "label")
       .attr("x",10)
-      .attr("y", function(_,i){ return i*25; })
-      .style("fill", function(d){ return colorMap(d)})
+      .attr("y", function(_,i){ return (labels.length -1) * 25 - i*25; })
+      .attr("fill", function(d){ return colorMap(d)})
       .text(function(d){ return d; })
       .attr("text-anchor", "left")
       .style("alignment-baseline", "middle");
 
+  legend.selectAll(".label").exit().remove();
+
 };
 
+
+async function update(stackedData, labels, x, y, colorMap) {
+  var series = d3.select("#series");
+  var legend = d3.select("#legend");
+
+  // Update color
+  series.selectAll(".serie").data(stackedData).transition().duration(1000)
+    .attr("fill", function(d) { return colorMap(d.key); });
+  
+    // Update height and y position
+  series.selectAll(".serie").selectAll(".bar")
+    .data(function(d) { return d; }).transition().duration(1000)
+    .attr("y", function(d) { return y(d[1]); })
+    .attr("height", function(d) { return y(d[0]) - y(d[1]); });
+
+  // Add one dot in the legend for each name.
+  legend.selectAll(".dot")
+    .data(labels).transition().duration(1000)
+      .attr("cy", function(_,i){ return (labels.length -1) * 25 - i * 25; })
+      .attr("fill", function(d){ return colorMap(d)});
+      
+      // Add one dot in the legend for each name.
+  legend.selectAll(".label")
+    .data(labels).transition().duration(1000)
+      .attr("y", function(_,i){ return (labels.length -1) * 25 - i * 25; })
+      .text(function(d){ return d; })
+      .attr("fill", function(d){ return colorMap(d)});
+
+}
+
+
 async function main() {
+  // load data
   var data = await loadData();
 
   // enumeration of the data-points
   var groups = d3.range(0, data.length);
 
   // list of subgroups = one data case -> one of the staked bars of the final chart
-  var subgroups = Object.getOwnPropertyNames(data[0]);
+  var subgroups = Object.keys(data[0]);
 
   const maxHeight = d3.max(d3.map(data, function(d){return d3.sum(Object.values(d))}));
 
@@ -107,45 +151,31 @@ async function main() {
   
   // color palette = one color per subgroup
   var colorMap = d3.scaleOrdinal().domain(data)
-    .range(d3.schemeRdYlBu[subgroups.length]);
+    .range(d3.schemeOrRd[subgroups.length]);
   
   // stack the data per subgroup
   var stackedData = d3.stack()
-    .keys(subgroups)(data);
-  
-  svg = d3.select("#stacked-bars");
+    .keys(subgroups).order(d3.stackOrderNone)(data);
 
-  // set the dimensions and margins of the graph
-  svg.attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
-  
-  makeChart(svg, stackedData, x, y, colorMap);
-  makeLegend(svg, subgroups, colorMap);
+  drawChart(stackedData, x, y, colorMap)
+  makeLegend(subgroups, colorMap);
 
-  d3.selectAll(".bar").on("click", function(event, i) {
-    var currentSerie = d3.select(this.parentNode);
-    var firstSerie = d3.select("#series").select(".serie");
+  d3.selectAll(".serie").on("click", function() {
+    var currKey = d3.select(this).datum().key;
+    var firstKey = subgroups[0];
 
-    firstKey = firstSerie.datum().key;
-    currentKey = currentSerie.datum().key;
-
-    currentSerie.transition().duration(1000).attr("fill", colorMap(firstSerie.datum().key));
-    firstSerie.transition().duration(1000).attr("fill", colorMap(currentSerie.datum().key));
-
-    currentSerie.datum().key = firstKey;
-    firstSerie.datum().key = currentKey;
-
-
-    // var tmp_data = currentSerie.datum().data;
-    // currentSerie.datum().data = firstSerie.datum().data;
-    // firstSerie.datum().data = tmp_data;
-
-    console.log(currentSerie.datum().key);
-    console.log(firstSerie.datum().key);
+    if (currKey == firstKey) {
+      return;
+    }
+    
+    swapIdx = subgroups.indexOf(currKey);
+    subgroups[0] = currKey;
+    subgroups[swapIdx] = firstKey;
+    
+    stackedData = d3.stack().keys(subgroups).order(d3.stackOrderNone)(data);
+    update(stackedData, subgroups, x, y, colorMap);
 
   });
-
-
 };
 
 main();
